ef4653863349887c8c7c178ef6614d71dc7bb364
shahanasha07e <shahanashaa07@gmail.comm>
2024-10-14 19:57:00 +0530

    patterns

---
+++ for.js
@@ -0,0 +1,12 @@
+
+
+//  for(i=0; i<10; i++){                         
+//     console.log("*")
+//  }
+ 
+
+//  var pattern=''
+//  for(i=0; i<10; i++){
+//     pattern+="*"
+//  }
+//  console.log(pattern)
