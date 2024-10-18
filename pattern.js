2f041073e7819ce4924e67915c7db91c0ca5ba73
shahanasha07e <shahanashaa07@gmail.comm>
2024-10-18 10:56:15 +0530

    patterns

---
+++ pattern.js
@@ -0,0 +1,25 @@
+// var star;
+//  for(i=1; i<=5; i++){
+//    star=" ";
+   
+//    for(j=1; j<=i; j++){
+//     star=star+"* ";
+//    }
+
+//    for(k=1; k>j; j--){
+//     star=" "+star;
+//    }
+//    console.log(star); 
+//  }
+
+
+
+//reverse
+let row=6;
+for(i=1; i<=row; i++){
+   let str="";
+   for(j=i;  j<=row; j++){
+      str=str+"* ";
+   }
+   console.log(str)
+}
